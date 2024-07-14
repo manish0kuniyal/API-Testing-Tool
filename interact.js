e1b15883a6d21d1c685443933fa55ee6b34d3742
manish0kuniyal <manishkuniyal24@gmail.com>
2024-07-14 22:32:46 +0530

    Connection

---
+++ interact.js
@@ -0,0 +1,9 @@
+const {Rest,Routes}=require('discord.js')
+
+    const commands=[
+       {
+        name:"ping",
+        description:"Replies with pong!",
+       },
+
+    ]
--- server.js
+++ server.js
@@ -1,12 +1,21 @@
 const express=require('express')
-const app=express()
+const{Client , GatewayIntentBits }=require('discord.js')
 
-app.use(express.json())
-
-app.get('/',(req,res)=>{
-    res.send({msg:"heel0"})
+const client=new Client({
+    intents:[
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent
+] 
+})
+client.on("messageCreate",(message)=>{
+    console.log(message)
+    if(message.author.bot)return
+    message.reply({
+        content:"hi from bot"
+    })
 })
 
-app.listen(3000,()=>{
-    console.log("...on port 3000")
-})
+client.login(
+    "MTI2MTY1NDcyOTcxNzcxMDk0MA.G0YUi_.oygQB6G6ki_orgLVkAtmBmzXkCb04cOSLJVzAM"
+)
