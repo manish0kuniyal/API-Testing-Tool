13a3a6f55d50942b8f47fef9ac72c50bf1b9c825
manish0kuniyal <manishkuniyal24@gmail.com>
2024-08-12 23:44:37 +0530

    frontend setupDashboard

--- interact.js
+++
@@ -1,9 +0,0 @@
-const {Rest,Routes}=require('discord.js')
-
-    const commands=[
-       {
-        name:"ping",
-        description:"Replies with pong!",
-       },
-
-    ]
---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className='border-2 border-green-200 p-3'>App</div>
+  )
+}
+
+export default App
